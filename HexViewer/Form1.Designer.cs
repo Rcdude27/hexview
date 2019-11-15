9e51b095b6c21c908e32ce55fd4fcfdd4b5082d2
Michael Wanek <michaelgwanek@gmail.com>
2019-11-15 11:01:38 -0600

    Add project files.

---
+++ HexViewer/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace HexViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.msMain = new System.Windows.Forms.MenuStrip();
+            this.miFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.miOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.miClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.miExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.nudStartParagraph = new System.Windows.Forms.NumericUpDown();
+            this.dgvContent = new System.Windows.Forms.DataGridView();
+            this.Paranum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Hex = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ASCII = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ofdOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.lblParagraph = new System.Windows.Forms.Label();
+            this.msMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudStartParagraph)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // msMain
+            // 
+            this.msMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miFile});
+            this.msMain.Location = new System.Drawing.Point(0, 0);
+            this.msMain.Name = "msMain";
+            this.msMain.Size = new System.Drawing.Size(602, 24);
+            this.msMain.TabIndex = 0;
+            this.msMain.Text = "menuStrip1";
+            // 
+            // miFile
+            // 
+            this.miFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miOpen,
+            this.miClose,
+            this.miExit});
+            this.miFile.Name = "miFile";
+            this.miFile.Size = new System.Drawing.Size(37, 20);
+            this.miFile.Text = "File";
+            // 
+            // miOpen
+            // 
+            this.miOpen.Name = "miOpen";
+            this.miOpen.Size = new System.Drawing.Size(180, 22);
+            this.miOpen.Text = "Open";
+            this.miOpen.Click += new System.EventHandler(this.miOpen_Click);
+            // 
+            // miClose
+            // 
+            this.miClose.Name = "miClose";
+            this.miClose.Size = new System.Drawing.Size(180, 22);
+            this.miClose.Text = "Close";
+            this.miClose.Click += new System.EventHandler(this.miClose_Click);
+            // 
+            // miExit
+            // 
+            this.miExit.Name = "miExit";
+            this.miExit.Size = new System.Drawing.Size(180, 22);
+            this.miExit.Text = "Exit";
+            this.miExit.Click += new System.EventHandler(this.miExit_Click);
+            // 
+            // nudStartParagraph
+            // 
+            this.nudStartParagraph.Enabled = false;
+            this.nudStartParagraph.Location = new System.Drawing.Point(74, 29);
+            this.nudStartParagraph.Name = "nudStartParagraph";
+            this.nudStartParagraph.Size = new System.Drawing.Size(80, 20);
+            this.nudStartParagraph.TabIndex = 1;
+            this.nudStartParagraph.ValueChanged += new System.EventHandler(this.nudStartParagraph_ValueChanged);
+            // 
+            // dgvContent
+            // 
+            this.dgvContent.AllowUserToAddRows = false;
+            this.dgvContent.AllowUserToDeleteRows = false;
+            this.dgvContent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvContent.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Paranum,
+            this.Hex,
+            this.ASCII});
+            this.dgvContent.Location = new System.Drawing.Point(13, 55);
+            this.dgvContent.Name = "dgvContent";
+            this.dgvContent.ReadOnly = true;
+            this.dgvContent.RowHeadersVisible = false;
+            this.dgvContent.Size = new System.Drawing.Size(574, 150);
+            this.dgvContent.TabIndex = 2;
+            // 
+            // Paranum
+            // 
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle10.Format = "N0";
+            dataGridViewCellStyle10.NullValue = null;
+            this.Paranum.DefaultCellStyle = dataGridViewCellStyle10;
+            this.Paranum.HeaderText = "Para #";
+            this.Paranum.Name = "Paranum";
+            this.Paranum.ReadOnly = true;
+            this.Paranum.Width = 50;
+            // 
+            // Hex
+            // 
+            dataGridViewCellStyle11.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Hex.DefaultCellStyle = dataGridViewCellStyle11;
+            this.Hex.HeaderText = "Hex";
+            this.Hex.Name = "Hex";
+            this.Hex.ReadOnly = true;
+            this.Hex.Width = 360;
+            // 
+            // ASCII
+            // 
+            dataGridViewCellStyle12.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ASCII.DefaultCellStyle = dataGridViewCellStyle12;
+            this.ASCII.HeaderText = "ASCII";
+            this.ASCII.Name = "ASCII";
+            this.ASCII.ReadOnly = true;
+            this.ASCII.Width = 160;
+            // 
+            // ofdOpenFile
+            // 
+            this.ofdOpenFile.AddExtension = false;
+            this.ofdOpenFile.SupportMultiDottedExtensions = true;
+            // 
+            // lblParagraph
+            // 
+            this.lblParagraph.AutoSize = true;
+            this.lblParagraph.Location = new System.Drawing.Point(12, 31);
+            this.lblParagraph.Name = "lblParagraph";
+            this.lblParagraph.Size = new System.Drawing.Size(56, 13);
+            this.lblParagraph.TabIndex = 3;
+            this.lblParagraph.Text = "Paragraph";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 220);
+            this.Controls.Add(this.lblParagraph);
+            this.Controls.Add(this.dgvContent);
+            this.Controls.Add(this.nudStartParagraph);
+            this.Controls.Add(this.msMain);
+            this.MainMenuStrip = this.msMain;
+            this.Name = "Form1";
+            this.Text = "Hex Viewer";
+            this.msMain.ResumeLayout(false);
+            this.msMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudStartParagraph)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip msMain;
+        private System.Windows.Forms.ToolStripMenuItem miFile;
+        private System.Windows.Forms.ToolStripMenuItem miOpen;
+        private System.Windows.Forms.ToolStripMenuItem miClose;
+        private System.Windows.Forms.ToolStripMenuItem miExit;
+        private System.Windows.Forms.NumericUpDown nudStartParagraph;
+        private System.Windows.Forms.DataGridView dgvContent;
+        private System.Windows.Forms.OpenFileDialog ofdOpenFile;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Paranum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Hex;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ASCII;
+        private System.Windows.Forms.Label lblParagraph;
+    }
+}
+
---
+++ HexViewer/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Pull in the classes for doing input and output.
+using System.IO;
+
+namespace HexViewer
+{
+    public partial class Form1 : Form
+    {
+        //Field to pull the file stream object for the currently opened file
+        private FileStream fsCurrFile = null; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void miExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // Format a sequence of 8 bytes for display, starting at the location iStart
+        // in the byte array byByteSeq. Each byte is written in hex, with a space
+        // between each pair of bytes.
+        private string strEightBytesToHex(byte[] byByteSeq, int iStart, int iNumBytes) {
+            // Start with the first byte.
+            string strOutput = byByteSeq[iStart].ToString("X2");
+            // Append the remaining bytes, separated by spaces.
+            for (int iLoc = iStart + 1; iLoc < iStart + iNumBytes; iLoc++) {
+                strOutput += " " + byByteSeq[iLoc].ToString("X2");
+            }
+            // Finished. Return string.
+            return strOutput;
+        }
+
+        // Format a paragraph for display. The bytes in the paragraph are written
+        // in hex, with a space between each pair of bytes. An extra space separates
+        // the first 8 bytes from the second 8 bytes.
+        private string strParaToHex(byte[] byParagraph, int iNumBytes) {
+            // Format the first 8 bytes in the paragraph.
+            string strOutput = strEightBytesToHex(byParagraph, 0, Math.Min(9, iNumBytes));
+            // Format the remaining 8 bytes and append to the output, with two
+            // spaces in between.
+            if (iNumBytes > 8)
+            {
+                strOutput += "  " + strEightBytesToHex(byParagraph, 8, iNumBytes - 8);
+            }
+            // Finished. Return string.
+            return strOutput;
+        }
+
+        // Convert a sequence of 8 bytes to ASCII text, representing bytes that are
+        // not printable ASCII characters with '.'.
+        private string strEightBytesToASCII(byte[] byByteSeq, int iStart, int iNumBytes) {
+            // Build up the output character by character, starting with "".
+            string strOutput = "";
+            // Loop through 8 bytes starting at iStart, converting each byte to
+            // ASCII (if possible) and appending to the output.
+            for (int iLoc = iStart; iLoc < iStart + iNumBytes; iLoc++) {
+                byte byOneByte = byByteSeq[iLoc];
+                if (0x20 <= byOneByte && byOneByte <= 0x7E) {
+                    strOutput += (char)byOneByte;
+                } else {
+                    strOutput += '.';
+                }
+            }
+            // Finished. Return string.
+            return strOutput;
+        }
+
+        // Convert a paragraph to ASCII text, as above. A space separates the first
+        // 8 bytes from the second 8 bytes.
+        private string strParaToASCII(byte[] byParagraph, int iNumBytes) {
+            // Convert the first 8 bytes, then the second 8 bytes, concatenating with
+            // a space in between.
+            string strOutput = strEightBytesToASCII(byParagraph, 0, Math.Min(8, iNumBytes));
+            strOutput += " " + strEightBytesToASCII(byParagraph, 8, iNumBytes - 8);
+            // Finished. Return string.
+            return strOutput;
+        }
+
+        private void miOpen_Click(object sender, EventArgs e)
+        {
+            //Display the open file dialog. Proceed to open the file only if the user
+            //clicked the "Open" button (dialog result is "OK")
+            DialogResult drOpenResult = ofdOpenFile.ShowDialog();
+            if (drOpenResult == DialogResult.OK)
+            {
+                //Open the file and set up the form to display that file's data
+                fsCurrFile = File.Open(ofdOpenFile.FileName, FileMode.Open, FileAccess.Read);
+                //Enable the numeric up-down and set up the max value so that choosing
+                //the max value displays the last 5 paragraphs in the file
+                nudStartParagraph.Enabled = true;
+                double dNumBytesInFile = fsCurrFile.Length;
+                double dNumParasInFile = Math.Ceiling(dNumBytesInFile / 16.0);
+                double dMaxParaNumInNUD = Math.Max(dNumParasInFile - 5.0, 0.0);
+                nudStartParagraph.Maximum = (decimal)dMaxParaNumInNUD;
+                //Set starting paragraph in numeric up-down to 0 so we start displaying
+                //bytes at the beginning of the file
+                nudStartParagraph.Value = 0;
+                //Display the first five paragraphs in the file
+                nudStartParagraph_ValueChanged(null, null);
+            }
+        }
+
+        private void nudStartParagraph_ValueChanged(object sender, EventArgs e)
+        {
+            if (fsCurrFile != null)
+            {
+                //Calc byte number to move to then do a seek to move the FPP
+                long lStartByteNum = (long)nudStartParagraph.Value * 16;
+                fsCurrFile.Seek(lStartByteNum, SeekOrigin.Begin);
+                //Read and Display 5 paragraphs from the file
+                //Clear the Data Grid View
+                dgvContent.Rows.Clear();
+                //Set up buffer for data read from file
+                byte[] byBuffer = new byte[16];
+                for (int iCount = 0; iCount < 5; iCount++)
+                {
+                    int iBytesRead = fsCurrFile.Read(byBuffer, 0, byBuffer.Length);
+                    //Only display if we read at least 1 byte
+                    if (iBytesRead > 0)
+                    {
+                        //Get the hex and text version of the paragraph data
+                        string strHex = strParaToHex(byBuffer, iBytesRead);
+                        string strText = strParaToASCII(byBuffer, iBytesRead);
+                        //Number of current paragraph is value from the NUD plus iCount
+                        decimal decParaNum = nudStartParagraph.Value + iCount;
+                        //Add a row to the data grid view
+                        dgvContent.Rows.Add(new object[] { decParaNum, strHex, strText });
+                    }
+                }
+            }
+        }
+
+        private void miClose_Click(object sender, EventArgs e)
+        {
+            //Close the current file, disable the NUD, and clear out the data grid view
+            if (fsCurrFile != null)
+            {
+                fsCurrFile.Close();
+                fsCurrFile = null;
+            }
+            nudStartParagraph.Value = 0;
+            nudStartParagraph.Enabled = false;
+            dgvContent.Rows.Clear();
+        }
+    }
+}
